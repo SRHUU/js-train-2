4e1ee8ca4e3e32f143fb2bb968d8312a2d857e29
SRHUU <sergiychervinko@gmail.com>
2023-09-26 12:54:41 +0300

    js-train-2 done

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ function calculateExpression(expression) {
   //Преревіряємо чи аргумент є рядком
   //Якщо так повертаємо виконананий код
   // Якщо ні повертаємо текст "Argument is not a string"
+  const result = eval(expression);
+  if (result !== undefined && result !== null) {
+    return result;
+  } else if (typeof expression !== "string") {
+    return "Argument is not a string";
+  }
 }
 
 console.log("Завдання 1 ====================================");
@@ -21,6 +27,13 @@ function getNumberFromString(s) {
   // Перевіряємо, чи є отримане значення числом за допомогою isNaN
   // Якщо значення не є числом, повертаємо текст "Couldn't extract a number from the provided string."
   // Якщо значення є числом, повертаємо його
+  s = parseFloat(s);
+
+  if (!isNaN(s)) {
+    return s;
+  } else {
+    return "Couldn't extract a number from the provided string.";
+  }
 }
 
 // Виводимо результати виклику функції
@@ -42,6 +55,13 @@ function getIntegerFromString(s) {
   // Перевіряємо, чи є отримане значення числом за допомогою isNaN
   // Якщо значення не є числом, повертаємо текст "Couldn't extract an integer from the provided string."
   // Якщо значення є числом, повертаємо його
+  s = parseFloat(s);
+
+  if (!isNaN(s)) {
+    return s;
+  } else {
+    return "Couldn't extract an integer from the provided string.";
+  }
 }
 
 // Виводимо результати виклику функції
@@ -64,6 +84,11 @@ function isNumberFinite(num) {
   // За допомогою оператора if перевіряємо, чи є число скінченним.
   // Якщо число є скінченним, повертаємо текст "The number is finite.".
   // Якщо число не є скінченним, повертаємо текст "The number is not finite.".
+  if (isFinite(num)) {
+    return "The number is finite.";
+  } else {
+    return "The number is not finite.";
+  }
 }
 
 // Виконуємо функцію з різними вхідними даними і виводимо результат.
@@ -80,6 +105,11 @@ function isValueNaN(value) {
   // За допомогою оператора if перевіряємо, чи є значення NaN.
   // Якщо значення є NaN, повертаємо текст "The value is NaN.".
   // Якщо значення не є NaN, повертаємо текст  "The value is not NaN.".
+  if (isNaN(value)) {
+    return "The value is NaN.";
+  } else {
+    return "The value is not NaN.";
+  }
 }
 
 // Виконуємо функцію з різними вхідними даними і виводимо результат.
@@ -94,6 +124,8 @@ function encodeURLComponent(urlComponent) {
   // Використовуємо вбудовану функцію encodeURIComponent, щоб закодувати введений компонент URL.
   // Ця функція повертає закодований компонент URL, замінивши небезпечні символи на їх процентне кодування.
   // Повертаємо закодований компонент URL.
+  urlComponent = encodeURIComponent(urlComponent);
+  return urlComponent;
 }
 
 // Виконуємо функцію з вхідними даними і виводимо результат.
@@ -110,6 +142,8 @@ function encodeURL(url) {
   // Використовуємо вбудовану функцію encodeURI, щоб закодувати введений URL.
   // Ця функція повертає закодований URL, замінивши небезпечні символи на їх процентне кодування.
   // Повертаємо закодований URL.
+  url = encodeURI(url);
+  return url;
 }
 
 // Виконуємо функцію з вхідними даними і виводимо результат.
@@ -126,6 +160,8 @@ function decodeURLComponent(urlComponent) {
   // Використовуємо вбудовану функцію decodeURIComponent, щоб декодувати введений закодований компонент URL.
   // Ця функція повертає декодований компонент URL, замінивши процентне кодування символів на їх реальні значення.
   // Повертаємо декодований компонент URL.
+  urlComponent = decodeURIComponent(urlComponent);
+  return urlComponent;
 }
 
 // Виконуємо функцію з вхідними даними і виводимо результат.
@@ -142,6 +178,8 @@ function decodeURL(url) {
   // Використовуємо вбудовану функцію decodeURI, щоб декодувати введений закодований URL.
   // Ця функція повертає декодований URL, замінивши процентне кодування символів на їх реальні значення.
   // Повертаємо декодований URL.
+  url = decodeURI(url);
+  return url;
 }
 
 // Виконуємо функцію з вхідними даними і виводимо результат.
